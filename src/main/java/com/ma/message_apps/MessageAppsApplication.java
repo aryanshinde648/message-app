bb3fcae0254531aad97f2f741aa14d5967db5920
sagarjate91 <jate.sagar91@gmail.com>
2025-08-08 15:31:50 +0530

    Updated code

--- src/main/java/com/ma/message_apps/MessageAppsApplication.java
+++ src/main/java/com/ma/message_apps/MessageAppsApplication.java
@@ -1,5 +1,14 @@
 package com.ma.message_apps;
 
+import com.ma.message_apps.entity.FriendRequests;
+import com.ma.message_apps.entity.Message;
+import com.ma.message_apps.entity.User;
+import com.ma.message_apps.enumDto.UserStatus;
+import com.ma.message_apps.repository.FriendRequestsRepository;
+import com.ma.message_apps.repository.MessageRepository;
+import com.ma.message_apps.repository.UserRepository;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
---
+++ src/main/java/com/ma/message_apps/entity/Message.java
@@ -0,0 +1,40 @@
+package com.ma.message_apps.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "messages")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Message {
+
+    @Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+    @Column(name = "message_id")
+    private Integer messageId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "sender_id", referencedColumnName = "user_id")
+    private User sender;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "receiver_id", referencedColumnName = "user_id")
+    private User receiver;
+
+    @Column(name = "message_text")
+    private String messageText;
+
+    @Column(name = "is_read")
+    private Boolean isRead;
+
+    @Column(name = "created_at")
+    private Timestamp timestamp;
+
+}
