aa06626144a243845a632053b6d9d802d6a9f157
sagarjate91 <jate.sagar91@gmail.com>
2025-08-13 17:40:05 +0530

    update some files

--- src/main/java/com/ma/message_apps/mapper/UserConversion.java
+++ src/main/java/com/ma/message_apps/mapper/UserConversion.java
@@ -4,9 +4,21 @@ import com.ma.message_apps.dto.UserDto;
 import com.ma.message_apps.entity.User;
 import org.mapstruct.Mapper;
 
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
 @Mapper(componentModel = "spring")
 public interface UserConversion {
 
     User toUserEntity(UserDto userDto);
     UserDto toUserDto(User user);
+
+
+    default Timestamp map(LocalDateTime value) {
+        return value == null ? null : Timestamp.valueOf(value);
+    }
+
+    default LocalDateTime map(Timestamp value) {
+        return value == null ? null : value.toLocalDateTime();
+    }
 }
--- src/main/java/com/ma/message_apps/repository/UserRepository.java
+++ src/main/java/com/ma/message_apps/repository/UserRepository.java
@@ -1,7 +1,12 @@
 package com.ma.message_apps.repository;
 
 import com.ma.message_apps.entity.User;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User,Integer> {
     /**
@@ -17,4 +22,8 @@ public interface UserRepository extends JpaRepository<User,Integer> {
      * @return true if exists, false otherwise
      */
     boolean existsByEmail(String email);
+
+    Optional<User> findByUsername(@NotBlank(message = "Username is required") @Size(min = 3, max = 50, message = "Username must be between 3 and 50 characters") String username);
+
+    Optional<User> findByEmail(@Email(message = "Email should be valid") @NotBlank(message = "Email is required") String email);
 }
